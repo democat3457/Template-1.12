7fa5bd276485510ad38e84915f3e296ed94cbb28
Colin Wong <cwongmath@gmail.com>
2021-02-09 14:50:18 -0600

    spacing

--- src/main/java/democat/example/ExampleMod.java
+++ src/main/java/democat/example/ExampleMod.java
@@ -16,24 +16,24 @@ import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 @Mod(modid = ExampleMod.MODID, name = ExampleMod.NAME, version = ExampleMod.VERSION)
 public class ExampleMod
 {
-    public static final String MODID = "examplemod";
-    public static final String NAME = "Example Mod";
-    public static final String VERSION = "0.0.1";
+	public static final String MODID = "examplemod";
+	public static final String NAME = "Example Mod";
+	public static final String VERSION = "0.0.1";
 	public static final Logger LOGGER = LogManager.getLogger(MODID);
-
-    @EventHandler
-    public void preInit(FMLPreInitializationEvent event)
-    {
+	
+	@EventHandler
+	public void preInit(FMLPreInitializationEvent event)
+	{
 		MinecraftForge.EVENT_BUS.register(this);
-    }
-
-    @EventHandler
-    public void init(FMLInitializationEvent event)
-    {
-        // some example code
-    }
-    
-    @SubscribeEvent
+	}
+	
+	@EventHandler
+	public void init(FMLInitializationEvent event)
+	{
+		// some example code
+	}
+	
+	@SubscribeEvent
 	public void onConfigChanged(OnConfigChangedEvent event) {
 		if (event.getModID().equals(ExampleMod.MODID)) {
 			ConfigManager.sync(ExampleMod.MODID, Config.Type.INSTANCE);
@@ -43,10 +43,8 @@ public class ExampleMod
 	
 	@Config(modid = ExampleMod.MODID, category = "catname")
 	public static class ExampleModConfig {
-		
 		@Config.Name("name")
 		@Config.Comment("comment")
 		public static boolean sample = true;
-		
 	}
 }
